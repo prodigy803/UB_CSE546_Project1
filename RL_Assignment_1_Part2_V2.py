89abefb265b174339ec94a9d7814d1befd55caf2
prodigy803 <pushkaraj803@gmail.com>
2021-09-28 19:47:03 -0400

    Final Deterministic Working

--- RL_Assignment_1_Part2_V2.py
+++ RL_Assignment_1_Part2_V2.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pprint import pprint
 import random
 
 class environment_q_learning:
     def __init__(self,type_of_env:str,gamma_disc: float, epsilon:float,epsilon_decay:float,learning_rate:float,no_of_episodes:int,max_time_steps:int):
         
-        self.chain_of_states = [[0,0]]
-        self.chain_of_actions = [0]
+        self.chain_of_states = []
+        self.chain_of_actions = []
+        self.chain_of_rewards = []
         if max_time_steps < 10:
             raise ValueError("Timesteps should be greater than of equal to 10")
         
@@ -47,9 +48,11 @@ class environment_q_learning:
 
         # this tells us the no_of_epsiodes during which we will determine the optimal q value
         self.no_of_episodes = no_of_episodes
+        self.current_episode = 1
         
         self.goal_pos = [4,4]
         self.agent_current_pos = [0,0]
+        self.done_or_not = False
 
         self.environment[tuple(self.agent_current_pos)] = 1
         self.environment[tuple(self.goal_pos)] = 0.5
@@ -72,13 +75,16 @@ class environment_q_learning:
         # Here we are essentially resetting all the values.
         self.current_time_steps = 0
         self.cumulative_reward = 0
+        self.chain_of_states = []
+        self.chain_of_actions = []
+        self.chain_of_rewards = []
 
         self.goal_pos = [4,4]
         self.agent_current_pos = [0,0]
         
         self.environment = np.zeros((5,5))
         
-        self.rewards = [{-1:[0,3]},{-1:[3,0]},{3:[2,3]},{3:[3,2]},{10:[4,4]}]
+        self.rewards = [{-1:[0,3]},{-1:[3,0]},{3:[2,3]},{3:[3,2]},{5:[4,4]}]
         self.reward_states = [(0,3),(3,0),(2,3),(3,2),[4,4]]
         
         for reward in self.rewards:
@@ -89,7 +95,7 @@ class environment_q_learning:
         self.environment[self.goal_pos] = 0.5
     
     def step(self):
-        print("Steps starting agent pos is", self.agent_current_pos)
+        # print("Steps starting agent pos is", self.agent_current_pos)
         # We are checking wether the environment is deterministic or stochastic
         if self.environment_type == 'deterministic':
             # In Deterministic environments, there is no use for epsilon as all the actions are deterministic / greedy / pre-determined.
@@ -101,38 +107,38 @@ class environment_q_learning:
             states_the_actions_lead_to = self.get_states_for_actions(all_possible_actions,self.agent_current_pos)
             # all_possible_actions -> [1,3] like that a single list
             # states_the_actions_lead_to -> [[1,0],[0,1]] -> states which the all_possible_actions lead to
+            # print(states_the_actions_lead_to)
             
-            dict_temp = {}
-            breaker = False
-            for action, state in zip(all_possible_actions, states_the_actions_lead_to):
-                dict_temp[action] = state
             
-            if any([self.check_and_get_reward(state)>0 for state in list(dict_temp.values())]):
-                maxi = max([self.check_and_get_reward(state) for state in list(dict_temp.values())])
-                for reward_counter in range(len(self.rewards)):
-                    for key, value in self.rewards[reward_counter].items():
-                        if key == maxi:
-                            self.chain_of_states.append(value)
-                            self.agent_current_pos = value
-                            breaker = True
-                    if breaker == True:
-                        break
-
-                for key, value in dict_temp.items():
-                    if self.agent_current_pos == value:
-                        self.chain_of_actions.append( key)
-
-            
-            elif all([self.check_and_get_reward(state) == 0 for state in list(dict_temp.values())]):
-                random_state = random.choice(list(dict_temp.values()))
+            # take a random_action:
+            if self.current_episode == 1:
                 
-                self.chain_of_states.append(random_state)
-                self.agent_current_pos = random_state
-
-                for key,value in dict_temp.items():
-                    if value == random_state:
-                        self.chain_of_actions.append(key)
-
+                random_action = random.choice(all_possible_actions)
+                dict_temp = {}
+                for action, state in zip(all_possible_actions,states_the_actions_lead_to):
+                    dict_temp[action] = state
+                
+                self.agent_current_pos = dict_temp[random_action]
+                self.chain_of_states.append(dict_temp[random_action])
+                self.chain_of_actions.append(random_action)
+                self.chain_of_rewards.append(self.check_and_get_reward(dict_temp[random_action]))
+            
+            elif self.current_episode > 1:
+                optimal_action, optimal_state,q_value_evaluated, q_values_evaluated = self.get_best_state_on_q_value(self.agent_current_pos,all_possible_actions,states_the_actions_lead_to)
+                # print('Agent Current State, ',self.agent_current_pos)
+                # print("All Possible Actions, ",all_possible_actions, "and their corresponding resultant states are ,",states_the_actions_lead_to)
+                # print('Optimal State Selected is ',optimal_state, "via ",self.agent_current_pos," with q value ", q_value_evaluated, " from ",q_values_evaluated)
+                
+                self.agent_current_pos = optimal_state
+                self.chain_of_states.append(optimal_state)
+                self.chain_of_actions.append(optimal_action)
+                if optimal_state == [4,4]:
+                    print('4,4 reached')
+                self.chain_of_rewards.append(self.check_and_get_reward(optimal_state))
+            if self.agent_current_pos == [4,4]:
+                print("reached optimal state in {} steps".format(self.current_time_steps))
+            elif [4,4] in environment_q_learning_obj.chain_of_states:
+                print("reached optimal state in {} steps".format(self.current_time_steps))
             breaker = False
             for reward_state_counter in range(len(self.rewards)):
                 for reward, state in self.rewards[reward_state_counter].items():
@@ -140,7 +146,7 @@ class environment_q_learning:
 
                     if state == self.agent_current_pos:
                         self.cumulative_reward += reward
-                        # del self.rewards[reward_state_counter]
+                        del self.rewards[reward_state_counter]
                         breaker = True
                         break
 
@@ -159,15 +165,54 @@ class environment_q_learning:
             
             # if the agent has reached the final state then done
             if (self.agent_current_pos == self.goal_pos) or (self.current_time_steps == self.max_timesteps):
-                done_or_not = True
+                self.done_or_not = True
             
             else:
-                done_or_not = False
-
-            return self.environment.flatten, self.cumulative_reward, done_or_not, self.current_time_steps
+                self.done_or_not = False
+            # print('Done or not floag', done_or_not)
+            return self.environment.flatten, self.cumulative_reward, self.done_or_not, self.current_time_steps
     
+    def get_best_state_on_q_value(self,current_state, all_possible_actions,states_the_actions_lead_to):
+        current_max = None
+        state_to_be_returned = None
+        action_to_be_returned = None
+        q_values_evaluated = None
+
+        for action,state in zip(all_possible_actions,states_the_actions_lead_to):
+            # print([4,4] in states_the_actions_lead_to)
+            if state == [4,4]:
+                return action, state,0, 0 
+
+            elif current_max == None:
+                current_max= self.qvalue_table[tuple(current_state)][action]
+                state_to_be_returned = state
+                action_to_be_returned = action
+                q_values_evaluated = self.qvalue_table[tuple(current_state)]
+
+            elif current_max != None:
+                max1 = self.qvalue_table[tuple(current_state)][action]
+                if max1 > current_max:
+                    current_max = max1
+                    state_to_be_returned = state
+                    action_to_be_returned = action
+                    q_values_evaluated = self.qvalue_table[tuple(current_state)]
+
+        return action_to_be_returned, state_to_be_returned, current_max, q_values_evaluated
+
+    def get_best_state_on_reward(self,all_possible_actions, states_the_actions_lead_to):
+        
+        for action,state in zip(all_possible_actions,states_the_actions_lead_to):
+            
+            for i in range(len(self.rewards)):
+                for key, value in self.rewards[i].items():
+                    if value == state:
+                        return action, state
+        return None, None
+                
+
+
     def get_state_based_on_action(self, action, state):
-        state_copy = state.copy()
+        state_copy = list(state).copy()
         if action == 0:
             # print('Up')
             state_copy[0] -=1
@@ -202,7 +247,7 @@ class environment_q_learning:
     # def get_all_q_values_for_states(self,all_actions,agent_state):
     def get_states_for_actions(self, all_actions, agent_state):
     
-        temp_states = agent_state.copy() 
+        temp_states = list(agent_state).copy() 
         # vals_to_be_returned = []
         states_considered = []
 
@@ -233,7 +278,7 @@ class environment_q_learning:
         elif (x_pos == 0) and (y_pos <= 3):
             return [1,2,3]
         
-        elif (x_pos <= 3) and (y_pos == 0 ):
+        elif (x_pos <= 3) and (y_pos == 0):
             return [0,1,3]
         
         elif (x_pos == 4) and (y_pos <= 3 ):
@@ -254,22 +299,48 @@ class environment_q_learning:
         plt.imshow(self.environment)
         plt.show()
 
+    def compute_q_values(self):
+        self.chain_of_actions.reverse(), self.chain_of_states.reverse(), self.chain_of_rewards.reverse()
+
+        for i in range(len(self.chain_of_rewards)-1):
+            # print("updating q value of ",self.chain_of_states[i+1])
+            
+            old_q_value = self.qvalue_table[tuple(self.chain_of_states[i+1])][self.chain_of_actions[i]]
+            
+            self.qvalue_table[tuple(self.chain_of_states[i+1])][self.chain_of_actions[i]] = old_q_value + self.learning_rate * (self.chain_of_rewards[i] + self.gamma * max(self.qvalue_table[tuple(self.chain_of_states[i])]) - old_q_value)
+
+            if tuple(self.chain_of_states[i]) == (4,4):
+                print(tuple(self.chain_of_states[i]))
+
     def train(self):
         # done = False
-        self.current_time_steps =0
+        
         for i in range(self.no_of_episodes):
-            print(i)
-            done = False
-            while not done:
-                # self.render()
-                observation, reward, done, _ = self.step()
-                
-                if done:
-                    print(reward)
+            self.current_time_steps = 0
+            self.done_or_not = False
             self.reset()
+            while not self.done_or_not:
+                # self.render()
+                # print(self.current_episode)
+                observation, reward, self.done_or_not, _ = self.step()
+                if self.done_or_not:
+                    print('Flag got set')
+                    print(reward)
+                # if self.done_or_not:
+                #     # print('flag got set')
+                #     # print(reward)
+                #     print(self.current_time_steps)
+                #     break
+            # self.render()
+            print(self.current_time_steps) 
+            self.compute_q_values()
+            
+            # print('Episode Completed')
+            # print('----')
+            self.current_episode+=1
+            # pprint(self.qvalue_table)
 
-
-# class environment_q_learning:
-#     def __init__(self,type_of_env:str,gamma_disc: float, epsilon:float,epsilon_decay:float,learning_rate:float,no_of_episodes:int,max_time_steps:int):
-environment_q_learning_obj = environment_q_learning(type_of_env = 'deterministic',gamma_disc=0.9,epsilon=0.2,epsilon_decay=0.001,learning_rate=0.1,no_of_episodes=1,max_time_steps=50)
+environment_q_learning_obj = environment_q_learning(type_of_env = 'deterministic',gamma_disc=0.9,epsilon=0.2,epsilon_decay=0.001,learning_rate=0.5,no_of_episodes=10,max_time_steps=1000)
 environment_q_learning_obj.train()
+pprint(environment_q_learning_obj.qvalue_table)
+pprint(environment_q_learning_obj.chain_of_states)
